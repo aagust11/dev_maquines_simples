5e8c59662ce4444547f9dbb71a697a5965ca9450
aagust11 <132393883+aagust11@users.noreply.github.com>
2025-10-18 17:43:01 +0200

    Bootstrap Momentor app for GitHub Pages

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/momentor/',
-  test: {
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts'
-  }
+  base: '/momentor/'
 });
